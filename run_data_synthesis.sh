c5bc54681c9d17cd45710d2b16ca6b6abdc16d4e
Xinle Deng <109469322+Xinle-Deng@users.noreply.github.com>
2024-09-15 19:42:08 +0800

    Update run_data_synthesis.sh

--- run_data_synthesis.sh
+++ run_data_synthesis.sh
@@ -30,6 +30,7 @@ output_dirs=("../output_1"
              "../output_8"
              "../output_9"
              "../output_10")
+world_knowledge_output_dir="../world_knowledge"
 merge_dir="../output"
 model="gpt-4o"
 num_templates_for_task=360
@@ -37,7 +38,18 @@ num_templates_for_task=360
 output_dirs_str=$(IFS=' '; echo "${output_dirs[*]}")
 length=${#api_keys[@]}
 
+# synthesis the world knowledge 
+echo "Stage 1: synthesize the world knowledge"
+python data_synthesis.py \
+    --api_key ${api_keys[0]} \
+    --base_url ${base_urls[0]} \
+    --output_dir ${world_knowledge_output_dir} \
+    --model ${model} \
+    --knowledge_dir "" \
+    --only_synthesize_world_knowledge "1"
+
 # add '&' to run commands in parallel
+echo "Stage 2: synthesize instruction-response templates"
 for ((i=0; i<$length; i++))
 do
   python data_synthesis.py \
@@ -46,16 +58,18 @@ do
     --output_dir ${output_dirs[i]} \
     --model ${model} \
     --num_templates_for_task ${num_templates_for_task} \
+    --knowledge_dir ${world_knowledge_output_dir} \
+    --only_synthesize_world_knowledge "0" \
     --seed ${i} & 
 done
 
 wait 
 
 # merge the results 
-echo "Merging the results..."
+echo "Stage 3: merge the templates"
 python merge_templates.py --input_dirs ${output_dirs_str} --output_dir ${merge_dir}
 # split templates for each task 
-echo "Splitting the templates..."
+echo "Stage 4: further filter and split the templates"
 python split_templates.py --output_dirs ${merge_dir} --model ${model}
 
 echo "Done!"
